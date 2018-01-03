5e18878847e1732600940745ae9bdd4676d834d6
june29 <june29.jp@gmail.com>
2018-01-04 01:00:22 +0900

    :sparkles: web3に触れるコードを書いてみる

---
+++ docs/2018-01/hello-metamask/app.js
@@ -0,0 +1,24 @@
+window.addEventListener('load', function() {
+  if (typeof web3 !== 'undefined') {
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    console.log('No web3? You should consider trying MetaMask!')
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
+
+  web3.version.getNetwork(function(err, netId) {
+    switch (netId) {
+      case "1":
+        console.log('This is mainnet')
+        break
+      case "2":
+        console.log('This is the deprecated Morden test network.')
+        break
+      case "3":
+        console.log('This is the ropsten test network.')
+        break
+      default:
+        console.log('This is an unknown network.')
+    }
+  });
+});
